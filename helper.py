1513128fb1c479613f67a735cc29e994d8aaf90c
Rianne Beuker <rianne.beuker@gmail.com>
2024-01-02 22:16:51 +0100

    Huiswerk les 10 Programmeren voor Beginners

--- helper.py
+++ helper.py
@@ -5,3 +5,20 @@ def decoreer(tekst=""):
     print(f"* {tekst} *")
     print(lengte * "*")
     print()
+
+def fooi_pp(bedrag,personen):
+   try:
+     bedrag_pp = bedrag / personen
+   except:
+      bedrag_pp = "??"
+   return f"Het bedrag aan fooi is per persoon {bedrag_pp}."
+
+def onderstreep(tekst=""):
+   uit = []
+   uit.append(tekst)
+   uit.append("=" * len(tekst))
+   
+   return uit
+
+def som(dictionary):
+   return sum(dictionary.values())
